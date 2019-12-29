e0b47d0d69f2fc44037997c025d95f99e517e7d1
Mapkn3 <79608236848@ya.ru>
2019-12-29 23:50:51 +0400

    Unified time picker for arrived and lunch time.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,42 +30,66 @@ class _MyHomePageState extends State<MyHomePage> {
   TimeOfDay _time = TimeOfDay(hour: 0, minute: 0);
   TimeOfDay _lunch = TimeOfDay(hour: 0, minute: 0);
 
-  void _getTime() async {
-    TimeOfDay selectedTime = await showTimePicker(
-      context: context,
-      initialTime: TimeOfDay.now(),
-      builder: (BuildContext context, Widget child) {
-        return MediaQuery(
-          data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
-          child: child,
-        );
-      },
-    );
-    setState(() {
-      if (selectedTime != null) {
-        _time = selectedTime;
-      }
-    });
-  }
-
-  Widget _lunchTimePicker() {
-    return CupertinoDatePicker(
-      initialDateTime: DateTime(1969, 1, 1, _lunch.hour, _lunch.minute),
-      onDateTimeChanged: (DateTime value) {
-        setState(() {
-          _lunch = TimeOfDay.fromDateTime(value);
+  Future<TimeOfDay> _getTime(TimeOfDay initTime) async {
+    TimeOfDay time = initTime;
+    String result = await showModalBottomSheet<String>(
+        context: context,
+        builder: (BuildContext builder) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 1.0, horizontal: 4.0),
+                          child: OutlineButton(
+                              child: Text('Cancel'),
+                              onPressed: () {
+                                Navigator.pop(context, '_getTime_cancel');
+                              }))),
+                  Expanded(
+                      child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 1.0, horizontal: 4.0),
+                          child: OutlineButton(
+                              child: Text('OK'),
+                              onPressed: () {
+                                Navigator.pop(context, '_getTime_ok');
+                              })))
+                ],
+              ),
+              Container(
+                height: MediaQuery.of(context).copyWith().size.height / 4,
+                child: CupertinoDatePicker(
+                  initialDateTime:
+                      DateTime(1969, 1, 1, initTime.hour, initTime.minute),
+                  onDateTimeChanged: (DateTime value) {
+                    time = TimeOfDay.fromDateTime(value);
+                  },
+                  mode: CupertinoDatePickerMode.time,
+                  use24hFormat: true,
+                ),
+              )
+            ],
+          );
         });
-      },
-      use24hFormat: true,
-      mode: CupertinoDatePickerMode.time,
-      backgroundColor: CupertinoDynamicColor.resolve(
-          CupertinoColors.systemBackground, context),
-    );
+    switch (result) {
+      case '_getTime_ok':
+        {
+          return time;
+        }
+      case '_getTime_cancel':
+      default:
+        {
+          return initTime;
+        }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    TimeOfDay arrivalTime = _time;
     int departureHour = _time.hour + _lunch.hour + 8;
     int departureMinute = _time.minute + _lunch.minute;
     int dayOverflow = departureHour ~/ 24;
@@ -76,79 +102,51 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'я пришёл на работу в',
-            ),
-            Row(
+      body: DefaultTextStyle(
+          style: Theme.of(context).textTheme.display1,
+          textAlign: TextAlign.center,
+          softWrap: true,
+          child: Center(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text(
-                  MaterialLocalizations.of(context).formatTimeOfDay(arrivalTime,
-                      alwaysUse24HourFormat: true),
-                ),
-                IconButton(
-                  icon: Icon(Icons.access_time),
-                  onPressed: _getTime,
-                ),
-              ],
-            ),
-            Text(
-              'я потратил на обед',
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  MaterialLocalizations.of(context).formatTimeOfDay(
-                      TimeOfDay(hour: _lunch.hour, minute: _lunch.minute),
-                      alwaysUse24HourFormat: true),
-                ),
-                IconButton(
-                  icon: Icon(Icons.access_time),
-                  onPressed: () {
-                    showCupertinoModalPopup(
-                        context: context,
-                        builder: (BuildContext builder) {
-                          return Column(
-                            children: <Widget>[
-                              Row(
-                                children: <Widget>[
-                                  CupertinoButton(
-                                      child: Text('Cancel'), onPressed: null),
-                                  CupertinoButton(
-                                      child: Text('OK'), onPressed: null)
-                                ],
-                              ),
-                              Container(
-                                height: MediaQuery.of(context)
-                                        .copyWith()
-                                        .size
-                                        .height /
-                                    4,
-                                child: _lunchTimePicker(),
-                              ),
-                            ],
-                          );
-                        });
+                Spacer(flex: 5),
+                Text('прибыл на работу'),
+                GestureDetector(
+                  child: Text(
+                    MaterialLocalizations.of(context)
+                        .formatTimeOfDay(_time, alwaysUse24HourFormat: true),
+                  ),
+                  onTap: () {
+                    _getTime(_time).then((TimeOfDay time) {
+                      setState(() {
+                        _time = time;
+                      });
+                    });
                   },
                 ),
+                Spacer(),
+                Text('обед занял'),
+                GestureDetector(
+                  child: Text(
+                    MaterialLocalizations.of(context)
+                        .formatTimeOfDay(_lunch, alwaysUse24HourFormat: true),
+                  ),
+                  onTap: () {
+                    _getTime(_lunch).then((TimeOfDay time) {
+                      setState(() {
+                        _lunch = time;
+                      });
+                    });
+                  },
+                ),
+                Spacer(),
+                Text(
+                    'можно уйти ${dayOverflow > 0 ? '${dayOverflow > 1 ? 'после' : ''}завтра' : ''} в ${MaterialLocalizations.of(context).formatTimeOfDay(departureTime, alwaysUse24HourFormat: true)}'),
+                Spacer(flex: 5),
               ],
             ),
-            Text(
-              'значит уйду ${dayOverflow > 0 ? '${dayOverflow > 1 ? 'после' : ''}завтра' : ''} в',
-              softWrap: true,
-            ),
-            Text(
-              MaterialLocalizations.of(context)
-                  .formatTimeOfDay(departureTime, alwaysUse24HourFormat: true),
-            ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
