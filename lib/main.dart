fea8c36974657d6c67cb9aaf113fc5ec87d9cc5e
Mapkn3 <79608236848@ya.ru>
2020-02-24 00:28:14 +0400

    Rename file to correct name.

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:when_home/model.dart';
+import 'package:when_home/timesheet.dart';
 
 import 'util.dart';
 
